587c3331c8905d0c96d23d907aa6caf9e1cbc7a3
Arin Novia <arinnoviaaa@gmail.com>
2022-12-11 18:42:11 +0700

    memperbaiki error class dan for di detail note

--- src/components/DetailNote.js
+++ src/components/DetailNote.js
@@ -138,23 +138,23 @@ const DetailNote = () => {
           <h1>CATEGORY</h1>
           {printElementSelect}
         </div>
-        <div class="container-addnote">
+        <div className="container-addnote">
           <div className="title-addnote" >
             <input type="text" name="titlenote" id="titlenote" className="input-field" placeholder="Title" value={title} onChange={handleTitleChange} />
-            <label for="titlenote" className="input-label">Title</label>
+            <label htmlFor="titlenote" className="input-label">Title</label>
           </div>
           <div className="note-columns" id="note-columns">
             <textarea id="note-columns-note" name="columns-note" className="input-notee" rows="15" cols="50" value={main} onChange={handleMainChange} />
-            <label for="columns-note" className="input-label-note">Notes </label>
+            <label htmlFor="columns-note" className="input-label-note">Notes </label>
           </div>
           <div className="cue-columns">
             <textarea id="cue-columns-note" name="cue-columns-note" className="input-cue" rows="15" cols="50" value={cue} onChange={handleCueChange} />
-            <label for="cue-columns-note" className="input-label-cue">Cue</label>
+            <label htmlFor="cue-columns-note" className="input-label-cue">Cue</label>
           </div>
           <div className="summary">
             <div className="summary-note">
               <textarea id="note-summary" type="text" name="summarynote" className="input-summary" placeholder="Summary" rows="8" value={summary} onChange={handleSummaryChange} />
-              <label for="summarynote" className="input-label-summary">Summary</label>
+              <label htmlFor="summarynote" className="input-label-summary">Summary</label>
             </div>
           </div>
           <div className="button-sec">
