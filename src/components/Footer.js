3fed627ec338b31d6a9d3601662d53db566419a6
Arin Novia <arinnoviaaa@gmail.com>
2022-12-11 21:20:03 +0700

    memperbaiki class error pada footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,16 +2,16 @@ import React from "react";
 
 function LandingPageFooter(){
     return(
-    <section class="footer">
-    <div class="box-container">
-        <div class="box">
+    <section className="footer">
+    <div className="box-container">
+        <div className="box">
             <h3>Navigation</h3>
             <a href="/">Home</a>
             <a href="/about">About</a>
             <a href="/login">Login</a>
         </div>
 
-        <div class="box">
+        <div className="box">
             <h3>Contact Us</h3>
             <a href="mailto:R243Y0478@dicoding.org">Noviana</a>
             <a href="mailto:R378Y0942@dicoding.org">Arin Novia</a>
@@ -19,7 +19,7 @@ function LandingPageFooter(){
             <a href="mailto:R182X0312@dicoding.org">Alfin Wahyu</a>
         </div>
 
-        <div class="box">
+        <div className="box">
             <h3>Other</h3>
             <a href="https://www.dicoding.com/" target="_blank" rel="noreferrer">Dicoding</a>
             <a href="https://kampusmerdeka.kemdikbud.go.id/" target="_blank" rel="noreferrer">Kampus Merdeka</a>
@@ -27,7 +27,7 @@ function LandingPageFooter(){
 
     </div>
 
-    <div class="credit"> Copyright © 2022  <span>   Note IT! </span> | All Rights Reserved </div>
+    <div className="credit"> Copyright © 2022  <span>   Note IT! </span> | All Rights Reserved </div>
 
 </section>
     )
